a0ae71e0cd2b9a6d2a7e9f9181277c6432da7257
MDC <maryrose.conquilla@gmail.com>
2025-04-20 10:12:50 +0000

    InstanceIniLesson

--- chapter1/ConstructorLesson.java
+++ chapter1/ConstructorLesson.java
@@ -15,3 +15,25 @@ class Chicken{
         name = "Duke";
     }
 }
+
+class Swan {
+    int numberEggs;
+    public static void main(String[] args) {
+       Swan mother = new Swan();
+       mother.numberEggs=1;  // writing Object fields
+       System.out.println("Swan number of eggs "+mother.numberEggs);   // reading
+    }
+}
+
+class Name{
+    String first ="Rose ";
+    String last="Conq";
+    String full = first + last;
+    public static void main(String[] args) {
+        Name name1 = new Name();
+        System.out.println(name1.full);
+        name1.first="Bilog";
+        name1.last="Dill";
+        System.out.println(name1.full);
+    }
+}
