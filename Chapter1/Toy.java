e04adfcbf97eb0cf80ca55c84efae192659998c8
MDC <maryrose.conquilla@gmail.com>
2025-04-20 03:25:15 +0000

    done adding output

--- Chapter1/Toy.java
+++ Chapter1/Toy.java
@@ -6,7 +6,13 @@ public class Toy {
     int quantity;
 
 public static void main(String[] args) {
-    
+    Toy toy1 = new Toy();
+    toy1.name="Labubu";
+    toy1.brand="Brand1";
+    toy1.price=1500;
+    toy1.quantity = 5;
 
+    toy1.price=1800;
+    System.out.println(toy1.price);
 }
 }
