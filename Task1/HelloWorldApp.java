0f752fe6e24fd9a2ce8e035b4f646e3545276432
MDC <maryrose.conquilla@gmail.com>
2025-04-28 04:50:41 +0000

    ArithmeticTask

--- Task1/HelloWorldApp.java
+++ Task1/HelloWorldApp.java
@@ -2,6 +2,6 @@ package Task1;
 
 public class HelloWorldApp {
     public static void main(String[] args) {
-        System.out.println("Kamusta Mundo!");
+       // System.out.println("Kamusta Mundo!");//
     }
 }
