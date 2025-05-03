90a4bc61d5115962bb4c7bc115aa79a519f5989b
MDC <maryrose.conquilla@gmail.com>
2025-05-03 14:13:52 +0000

    Task2

--- chapter2/Task2.java
+++ chapter2/Task2.java
@@ -2,8 +2,18 @@ package chapter2;
 
 public class Task2 {
    public static void main(String[] args) {
-    byte zero =0;
-    String output = "W" + zero + "w";
+    String s1 = "H";
+    int three = 3;
+    byte one = 1;
+    short oneone = 1;
+    byte zero = 0;
+    char w1 = 'w';
+    int x = 0;
+    String s2 = "rld";
+    float pi = 2.0F;
+
+    boolean t = one == oneone;
+    String output = s1 + three + one + oneone + zero + " " + w1 + x + s2 + " " + pi + " " + t;
     System.out.println(output);
    } 
 }
