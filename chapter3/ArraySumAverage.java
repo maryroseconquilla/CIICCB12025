211d122424ed493fbf1ebed390e21e96780748a0
MDC <maryrose.conquilla@gmail.com>
2025-05-05 03:53:27 +0000

    ArraySumAverage

--- chapter3/ArraySumAverage.java
+++ chapter3/ArraySumAverage.java
@@ -1,14 +1,15 @@
 package chapter3;
-
+import java.util.*;;
 public class ArraySumAverage {
     public static void main(String[] args) {
         int[] numbers = {91, 82, 93, 84, 75};
 
-        int sum = 0;
-        for (int number : numbers) {
-            sum += number;
-        }
+        //int sum = 0;
+        //for (int number : numbers) {
+        //    sum += number;
+        //}
 
+        int sum = Arrays.stream(numbers).sum();
         double average = (double) sum / numbers.length;
 
         System.out.println("Sum: " + sum);
