73e82e4e64cca814999febddf916e421ab78fac3
MDC <maryrose.conquilla@gmail.com>
2025-05-13 03:27:23 +0000

    May13

---
+++ chapter4/Glider2.java
@@ -0,0 +1,6 @@
+public class Glider2 {
+    public static String glide(String s) { return "1";}
+public static String glide(String... s) { return "2";}
+public static String glide(Object o) { return "3";}
+public static String glide(String s, String t) { return "4";}
+}
