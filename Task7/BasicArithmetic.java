6b43557671352849aa4124fff67a332892ba4597
MDC <maryrose.conquilla@gmail.com>
2025-05-16 03:26:38 +0000

    BasicArithmetic

--- Task7/BasicArithmetic.java
+++ Task7/BasicArithmetic.java
@@ -22,23 +22,15 @@ public class BasicArithmetic {
         return sum;
     }
 
-    // Method with a String return type
-    public static String greet(String name) {
-        return "Hello, " + name + "!";
-    }
-
 
     public static void main(String[] args) {
         int resultSum = add(5, 3);
         int resultMinus = minus(5, 3);
         int resultMultiply = multiply(5, 3);
         int resultDivide = divide(5, 3);
-        System.out.println("The sum is: " + resultSum); // Output: The sum is: 8
-        System.out.println("The sum is: " + resultMinus); // Output: The sum is: 2
-        System.out.println("The sum is: " + resultMultiply); // Output: The sum is: 15
-        System.out.println("The sum is: " + resultDivide); // Output: The sum is: 1
-
-        String greetingMessage = greet("Task7");
-        System.out.println(greetingMessage); // Output: Hello, Task7!
+        System.out.println("Addition: " + resultSum); // Output: The sum is: 8
+        System.out.println("Substraction: " + resultMinus); // Output: The sum is: 2
+        System.out.println("Multiplication: " + resultMultiply); // Output: The sum is: 15
+        System.out.println("Division: " + resultDivide); // Output: The sum is: 1
     }
 }
