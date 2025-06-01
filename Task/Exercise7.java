227cfc289deb71047f00e489c1af7e3959f55479
MDC <maryrose.conquilla@gmail.com>
2025-06-01 14:59:03 +0000

    NumberToMonth

--- Task/Exercise7.java
+++ Task/Exercise7.java
@@ -1,31 +1,10 @@
 package Task;
-import java.time.Month;
-import java.time.format.TextStyle;
-import java.util.Locale;
+
 import java.util.Scanner;
+public class Exercise7 {
 
-public class NumberToMonth {
-    //public static void main(String[] args) {
-
-    //    Scanner input = new Scanner(System.in); // Create a Scanner object
-
-    //    System.out.print("Enter the number of month: "); // Prompt the user for input
-    //    int monthNumber = input.nextInt(); // Read the integer grade from the user
-
-        //int monthNumber = 7; // Represents July
-
-        // Get the Month enum instance for the given number
-    //    Month month = Month.of(monthNumber);
-
-        // Get the full display name of the month in English
-    //    String monthName = month.getDisplayName(TextStyle.FULL, Locale.ENGLISH);
-
-    //    System.out.println("Month number " + monthNumber + " is " + monthName);
-    //}
-
-
-
-public static void main(String[] strings) {
+    
+  public static void main(String[] strings) {
 
         Scanner input = new Scanner(System.in);
 
@@ -93,9 +72,5 @@ public static void main(String[] strings) {
         }
         System.out.print(MonthOfName + " " + year + " has " + number_Of_DaysInMonth + " days\n");
     }
-
-
-
-
-    
 }
+
